bd1f69bd289ff1256bcafffb4e32c558a5a6328d
OrestHrynyshyn <ads.hrynyshyn@gmail.com>
2021-03-24 17:19:12 +0200

    initial commit

---
+++ homework-4/main.js
@@ -0,0 +1,367 @@
+//Завдання №1
+// let greeting = 'hello';
+// let domain1 = 'owu';
+// let domain2 = 'com';
+// let domain3 = 'ua';
+// let number1 = 1;
+// let number2 = 10;
+// let number3 = -999;
+// let number4 = 123;
+// let number5 = 3.14 ;
+// let number6 = 16;
+// let result1 = true;
+// let result2 = false;
+//
+// console.log(greeting);
+// console.log(domain1);
+// console.log(domain2);
+// console.log(domain3);
+// console.log(number1);
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+// console.log(number4);
+// console.log(number5);
+// console.log(number6);
+// console.log(result1);
+// console.log(result2);
+
+// document.write(`${greeting}-${domain1}-${domain2}-${domain3}-${number1}-${number2}-${number3}-${number4}-${number5}-${number6}-${result1}-${result2}`);
+// document.write(greeting);
+// document.write(domain1);
+// document.write(domain2);
+// document.write(domain3);
+// document.write(number1);
+// document.write(number1);
+// document.write(number2);
+// document.write(number3);
+// document.write(number4);
+// document.write(number5);
+// document.write(number6);
+// document.write(result1);
+// document.write(result2);
+
+// alert(`${greeting} ${domain1} ${domain2} ${domain3} ${number1} ${number2} ${number3} ${number4} ${number5} ${number6} ${result1} ${result2}`);
+// alert(greeting);
+// alert(domain1);
+// alert(domain2);
+// alert(domain3);
+// alert(number1);
+// alert(number1);
+// alert(number2);
+// alert(number3);
+// alert(number4);
+// alert(number5);
+// alert(number6);
+// alert(result1);
+// alert(result2);
+
+
+
+//Завдання №2
+// greeting = 'hi';
+// domain1 = 'okten';
+// domain2 = 'web';
+// domain3 = 'com';
+// number1 = 2;
+// number2 = 20;
+// number3 = 666;
+// number4 = -123;
+// number5 = 4.1 ;
+// number6 = 160;
+// result1 = false;
+// result2 = true;
+//
+// console.log(`${greeting}-${domain1}-${domain2}-${domain3}-${number1}-${number2}-${number3}-${number4}-${number5}-${number6}-${result1}-${result2}`);
+// document.write(`  ${greeting}-${domain1}-${domain2}-${domain3}-${number1}-${number2}-${number3}-${number4}-${number5}-${number6}-${result1}-${result2} `);
+// alert(`${greeting} ${domain1} ${domain2} ${domain3} ${number1} ${number2} ${number3} ${number4} ${number5} ${number6} ${result1} ${result2}`);
+
+
+
+//Завдання №3
+// const firstNumber = 2;
+// const secondNumber = 20;
+// const thirdNumber = 200;
+// const firstWord = 'Hi';
+// const secondWord = 'Hello';
+// const thirdWord = 'World';
+//
+// console.log(firstNumber);
+// console.log(secondNumber);
+// console.log(thirdNumber);
+// console.log(firstWord);
+// console.log(secondWord);
+// console.log(thirdWord);
+//
+// document.write(firstNumber);
+// document.write(secondNumber);
+// document.write(thirdNumber);
+// document.write(firstWord);
+// document.write(secondWord);
+// document.write(thirdWord);
+
+// alert(firstNumber);
+// alert(secondNumber);
+// alert(thirdNumber);
+// alert(firstWord);
+// alert(secondWord);
+// alert(thirdWord);
+
+
+
+//Завдання №4
+// let name = prompt('Write name');
+// let surname = prompt('Write surname');
+// let fatherName = prompt('Write fatherName');
+//
+// console.log(surname);
+// console.log(name);
+// console.log(fatherName);
+//
+// document.write(surname);
+// document.write(name);
+// document.write(fatherName);
+
+// alert(surname);
+// alert(name);
+// alert(fatherName);
+
+
+
+//Завдання №5
+// var person = (`${surname} ${name} ${fatherName}`);
+// var person = (surname + ' ' + name + ' ' + fatherName);
+// console.log(person);
+
+
+
+//Завдання №6
+// let name1 = prompt('Enter your dad"s name');
+// let surname1 = prompt('Enter your dad"s surname');
+// let fatherName1 = prompt('Enter your dad"s fatherName');
+//
+// var person1 = (`${surname1} ${name1} ${fatherName1}`);
+// console.log(person1);
+//
+// let name2 = prompt('Enter your mother"s name');
+// let surname2 = prompt('Enter your mother"s surname');
+// let fatherName2 = prompt('Enter your mother"s fatherName');
+//
+// var person2 = (`${surname2} ${name2} ${fatherName2}`);
+// console.log(person2);
+//
+// let name3 = prompt('Enter your sister"s name');
+// let surname3 = prompt('Enter your sister"s surname');
+// let fatherName3 = prompt('Enter your sister"s fatherName');
+//
+// var person3 = (`${surname3} ${name3} ${fatherName3}`);
+// console.log(person3);
+
+
+
+//Завдання №7
+// let reNumber1 = +prompt('Enter number');
+// let reNumber2 = +prompt('Enter number');
+// let reNumber3 = +prompt('Enter number');
+//
+// console.log(reNumber1);
+// console.log(reNumber2);
+// console.log(reNumber3);
+
+
+
+//Завдання №8
+// let newNumber1 = prompt('Enter number 1');
+// let newNumber2 = prompt('Enter number 2');
+// let newNumber3 = prompt('Enter number 3');
+// let newNumber4 = prompt('Enter number 4');
+//
+// newNumber1 = parseInt(newNumber1);
+// newNumber2 = parseInt(newNumber2);
+// newNumber3 = parseInt(newNumber3);
+// newNumber4 = parseInt(newNumber4);
+//
+// console.log(newNumber1);
+// console.log(newNumber2);
+// console.log(newNumber3);
+// console.log(newNumber4);
+//
+// let result = newNumber1 + newNumber2 + newNumber3 + newNumber4;
+// console.log(result);
+
+
+//Завдання №9
+// let newNumber5 = prompt('Enter number 1');
+// let newNumber6 = prompt('Enter number 2');
+// let newNumber7 = prompt('Enter number 3');
+//
+// newNumber5 = parseFloat(newNumber5);
+// newNumber6 = parseFloat(newNumber6);
+// newNumber7 = parseFloat(newNumber7);
+//
+//
+// result = newNumber5 + newNumber6 + newNumber7;
+// console.log(result);
+
+
+//Завдання №10
+// let newNumber8 = +prompt('Enter number 1');
+// let newNumber9 = +prompt('Enter number 2');
+// let newNumber10 = +prompt('Enter number 3');
+//
+// newNumber8 = Math.round(newNumber8);
+// newNumber9 = Math.round(newNumber9);
+// newNumber10 = Math.round(newNumber10);
+//
+//
+// results = newNumber8 + newNumber9 + newNumber10;
+// console.log(results);
+
+
+
+//Завдання №11
+// let number11 = prompt('Enter number');
+// let number12 = prompt('Enter power');
+//
+// number11 = Math.round(number11);
+// number12 = Math.round(number12);
+//
+// number11 = Math.pow(number11,number12);
+// console.log(number11);
+
+
+
+//Завдання №12
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+
+//Завдання №13
+// let type1 = 5 < 6;
+// console.log(type1);
+// let type2 = 5 > 6;
+// console.log(type2);
+// let type3 = 5 >= 6;
+// console.log(type3);
+// let type4 = 5 == 6;
+// console.log(type4);
+// let type5 = 10 === 10;
+// console.log(type5);
+// let type6 = 10 == 10;
+// console.log(type6);
+// let type7 = 10 > 10;
+// console.log(type7);
+// let type8 = 10 < 10;
+// console.log(type8);
+// let type9 = 10 !== 10;
+// console.log(type9);
+// let type10 = 123 === '123';
+// console.log(type10);
+// let type11 = 123 == '123';
+// console.log(type11);
+
+
+//Завдання №14 (Додатково)
+// console.log(132 > 100 && 45 < 12 ); false
+// console.log(34 > 33 && 23 < 90 ); true
+// console.log(99 > 100 && 45 > 12 ); false
+// console.log(132 > 100 || 45 < 12 ); true
+// console.log(111 > 11 || 45 < 111 ); true
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); true
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); true
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); false
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); true
+// console.log(!!'-1'); true
+// console.log(!!-1); true
+// console.log(!!'0'); true
+// console.log(!!'null'); true
+// console.log(!!'undefined'); true
+// console.log(!!(3/'owu')); false
+// console.log((111 > 11 || 45 < 111) ||  !!'0'); true
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); false
+
+
+
+
+
+//=====================
+// ======ДОП============
+// =====================
+//Завдання №1
+// let nubmerOne = +prompt('Enter the first number');
+// let nubmerTwo = +prompt('Enter the second number');
+// let nubmerThird = +prompt('Enter the third number');
+//
+// if (nubmerOne > nubmerTwo && nubmerOne > nubmerThird) {
+//     if (nubmerTwo > nubmerThird) {
+//         console.log(nubmerThird, nubmerTwo, nubmerOne);
+//     } else {
+//         console.log(nubmerTwo, nubmerThird, nubmerOne);
+//     }
+// } else
+//       if (nubmerTwo > nubmerOne && nubmerTwo > nubmerThird) {
+//           if (nubmerOne > nubmerThird) {
+//               console.log(nubmerThird, nubmerOne, nubmerTwo);
+//           } else {
+//               console.log(nubmerOne, nubmerThird, nubmerTwo);
+//           }
+//       } else
+//           if (nubmerThird > nubmerOne && nubmerThird > nubmerTwo) {
+//               if (nubmerOne > nubmerTwo) {
+//                   console.log(nubmerTwo, nubmerOne, nubmerThird);
+//               } else {
+//                   console.log(nubmerOne, nubmerTwo, nubmerThird);
+//               }
+//           }
+
+
+//Завдання №2
+// let status = prompt('Ввведіть колір світлофора з запропонованих: "зелений", "жовтий", "червоний", "аварійний режим"', 'зелений');
+// if (status == 'зелений') {
+//     console.log('йди');
+// } else
+//     if (status == 'жовтий') {
+//         console.log('зачекай');
+//     } else
+//         if (status == 'червоний') {
+//             console.log('стій')
+//         } else
+//             if (status == 'аварійний режим') {
+//                 console.log('роби що хочеш')
+//             }
+
+
+//Завдання №3
+// let status = prompt('Ввведіть колір світлофора з запропонованих: "зелений", "жовтий", "червоний", "аварійний режим"', 'зелений');
+// let isRoadClear = confirm('Є машина?')
+// if (status == 'зелений' && isRoadClear == true) {
+//     console.log('йди');
+// } else
+//     if (status == 'зелений' && isRoadClear == false){
+//     console.log('зачекай поки порушники проїдуть');
+// }
+//
+//     if (status == 'жовтий' && isRoadClear == true) {
+//             console.log('зачекай');
+//         } else
+//             if (status == 'жовтий' && isRoadClear == false){
+//             console.log('всерівно зачекай');
+//         }
+//
+//     if (status == 'червоний' && isRoadClear == true) {
+//             console.log('стій і чекай')
+//         } else
+//             if (status == 'червоний' && isRoadClear == false){
+//             console.log('стій всеодно')
+//         }
+//
+//     if (status == 'аварійний режим') {
+//             console.log('роби що хочеш')
+//         }
